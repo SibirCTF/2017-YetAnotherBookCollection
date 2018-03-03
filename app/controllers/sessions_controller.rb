473b0f72ad0bf3b7b3e04d80f2c8bc1fdfa2b7d3
IgorPolyakov <igorpolyakov@protonmail.com>
2018-03-03 14:09:55 +0700

    fix by rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by(email: params[:email])
     # If the user exists AND the password entered is correct.
     if user&.password == params[:password]
       # Save the user id inside the browser cookie. This is how we keep the user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
-class UsersController < ApplicationController
-    def new
-    end
+# frozen_string_literal: true
 
-    def create
-      user = User.new(user_params)
-      if user.save
-        session[:user_id] = user.id
-        redirect_to '/'
-      else
-        redirect_to '/signup'
-      end
+class UsersController < ApplicationController
+  def new; end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
     end
-    def admin
-      @books = Book.all
-      @users = User.all
-    end
-    def user_params
-      params[:user]
-    end
+  end
+
+  def admin
+    @books = Book.all
+    @users = User.all
+  end
+
+  def user_params
+    params[:user]
+  end
 end
